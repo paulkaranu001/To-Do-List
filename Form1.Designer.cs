031851953f76df32025a08241440a5c73fcb00f7
paulkaranu001 <paulkaranu@live.com>
2024-08-13 02:18:51 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace To_Do_List
+{
+    partial class toDoList
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            titleTextBox = new TextBox();
+            descriptionTextBox = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            newbutton = new Button();
+            editButton = new Button();
+            deleteButton = new Button();
+            SaveButton = new Button();
+            toDoListView = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)toDoListView).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Georgia", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(21, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(1329, 77);
+            label1.TabIndex = 0;
+            label1.Text = "To Do List ";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.Click += label1_Click;
+            // 
+            // titleTextBox
+            // 
+            titleTextBox.Location = new Point(21, 109);
+            titleTextBox.Name = "titleTextBox";
+            titleTextBox.Size = new Size(1329, 27);
+            titleTextBox.TabIndex = 1;
+            // 
+            // descriptionTextBox
+            // 
+            descriptionTextBox.Location = new Point(21, 167);
+            descriptionTextBox.Name = "descriptionTextBox";
+            descriptionTextBox.Size = new Size(1329, 27);
+            descriptionTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.Location = new Point(21, 136);
+            label2.Name = "label2";
+            label2.Size = new Size(325, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Description:";
+            // 
+            // label3
+            // 
+            label3.Location = new Point(21, 71);
+            label3.Name = "label3";
+            label3.Size = new Size(325, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Title:";
+            label3.Click += label3_Click;
+            // 
+            // newbutton
+            // 
+            newbutton.Location = new Point(21, 216);
+            newbutton.Name = "newbutton";
+            newbutton.Size = new Size(300, 51);
+            newbutton.TabIndex = 5;
+            newbutton.Text = "New";
+            newbutton.UseVisualStyleBackColor = true;
+            newbutton.Click += newbutton_Click;
+            // 
+            // editButton
+            // 
+            editButton.Location = new Point(374, 216);
+            editButton.Name = "editButton";
+            editButton.Size = new Size(300, 51);
+            editButton.TabIndex = 6;
+            editButton.Text = "EDIT";
+            editButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            deleteButton.Location = new Point(716, 216);
+            deleteButton.Name = "deleteButton";
+            deleteButton.Size = new Size(300, 51);
+            deleteButton.TabIndex = 7;
+            deleteButton.Text = "DELETE";
+            deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // SaveButton
+            // 
+            SaveButton.Location = new Point(1051, 216);
+            SaveButton.Name = "SaveButton";
+            SaveButton.Size = new Size(300, 51);
+            SaveButton.TabIndex = 8;
+            SaveButton.Text = "SAVE";
+            SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // toDoListView
+            // 
+            toDoListView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            toDoListView.BackgroundColor = Color.White;
+            toDoListView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            toDoListView.Location = new Point(21, 288);
+            toDoListView.Name = "toDoListView";
+            toDoListView.RowHeadersWidth = 51;
+            toDoListView.Size = new Size(1346, 588);
+            toDoListView.TabIndex = 9;
+            // 
+            // toDoList
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gray;
+            ClientSize = new Size(1362, 872);
+            Controls.Add(toDoListView);
+            Controls.Add(SaveButton);
+            Controls.Add(deleteButton);
+            Controls.Add(editButton);
+            Controls.Add(newbutton);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(descriptionTextBox);
+            Controls.Add(titleTextBox);
+            Controls.Add(label1);
+            Name = "toDoList";
+            Text = "To Do List ";
+            Load += toDoList_Load;
+            ((System.ComponentModel.ISupportInitialize)toDoListView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox titleTextBox;
+        private TextBox descriptionTextBox;
+        private Label label2;
+        private Label label3;
+        private Button newbutton;
+        private Button editButton;
+        private Button deleteButton;
+        private Button SaveButton;
+        private DataGridView toDoListView;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace To_Do_List
+{
+    public partial class toDoList : Form
+    {
+        public toDoList()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toDoList_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newbutton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
